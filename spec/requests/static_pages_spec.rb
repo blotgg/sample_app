20a818892cdbb8872233b8185ef18b7e666464a9
harizm <blotgg@gmail.com>
2013-07-14 18:09:08 +0400

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,61 +1,38 @@
 require 'spec_helper'
+require 'support/utilities.rb'
 
 describe "Static pages" do
   
   let(:base_title) { "Бублег" }
+  subject { page }
 
   describe "Home page" do
+    before { visit root_path }
 
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Sample App')
-    end
-    
-    it "Проверка базового титла" do
-      visit '/static_pages/home'
-      page.should have_title("#{base_title}")
-    end
-    
-    it "Проверка отсутствия титла '| Home' титла" do
-      visit '/static_pages/home'
-      page.should_not have_title(" | Home")
-    end
+    it { should have_selector('h1', :text => 'Sample App') }
+    it { should have_title( full_title('') ) }
+    it { should_not have_title '| Home' }
   end
   
   describe "Help page" do
-    it "Проверка контента на наличие 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('h1', :text => 'Help')
-    end
+    before { visit help_path }
     
-    it "Проверка титла 'Help'" do
-      visit '/static_pages/help'
-      page.should have_title("#{base_title} | Help")
-    end
-    
+    it { should have_selector('h1', :text => 'Help') }   
+    it { should have_title full_title('Help') }
   end
   
   describe "About page" do
-    it "Проверка контента на наличие 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_selector('h1', :text => 'About Us')
-    end
+    before { visit about_path }
     
-    it "Проверка титла 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_title("#{base_title} | About Us")
-    end
+    it { should have_selector('h1', :text => 'About Us') }
+    it { should have_title full_title('About Us') }
   end
   
   describe "Contact" do
-    it "Проверка контента на наличие 'Contact'" do
-      visit '/static_pages/contact'
-      page.should have_selector('h1', :text => 'Contact')
-    end
+    before { visit contact_path }
     
-    it "Проверка титла 'Contact'" do
-      visit '/static_pages/contact'
-      page.should have_title("#{base_title} | Contact")
-    end
+    it { should have_selector('h1', :text => 'Contact') }
+    it { should have_title full_title('Contact') }
   end
+  
 end
