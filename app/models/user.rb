11a36ababb5865872ca227ad0fb199d5da62a617
harizm <blotgg@gmail.com>
2013-08-08 18:38:26 +0400

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,12 @@ class User < ActiveRecord::Base
   
   has_secure_password
   has_many :microposts, dependent: :destroy #dependent: :destroy - означает удалять связанные сообщения при удалении юзера
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+  has_many :reverse_relationships, foreign_key: "followed_id",
+                                   class_name:  "Relationship",
+                                   dependent:   :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
   
   #before_save { |user| user.email = email.downcase } #альтерантива этой колбэк функции:
   before_save { email.downcase! }
@@ -28,7 +34,19 @@ class User < ActiveRecord::Base
 
   def feed
     # Это предварительное решение. См. полную реализацию в "Following users".
-    Micropost.where("user_id = ?", id)
+    Micropost.from_users_followed_by(self)
+  end
+  
+  def following?(other_user)
+    relationships.find_by_followed_id(other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+  
+  def unfollow!(other_user)
+    relationships.find_by_followed_id(other_user.id).destroy
   end
   
   private
